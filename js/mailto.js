6f6083d1deb42c3fa3aab39c204053fb4b6875a6
Eri Gamo <erigamo@Eris-MBP.fios-router.home>
2019-09-24 04:43:25 -0400

    new version of splash with paper and link to port

--- js/mailto.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("prevent_spam").addEventListener("click", function(){ 
-    var username = "gamoeri", 
-          domain = "google";
-    window.location.href = "mailto:" + username + "@" + domain + ".com";
-} );
